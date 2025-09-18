c9ef9ae99349dcb936e510bd40b0069f10a6344c
JASIR K A <jasirhuzain@gmail.com>
2025-09-18 16:07:42 +0530

    Time: 0 ms (100%), Space: 40.7 MB (28.65%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static int climbStairs(int n) {
+        if (n <= 1) return 1;
+
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = curr + prev;
+            prev = temp;
+        }
+        return curr;
+    }
+}
+
+    
